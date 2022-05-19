048ea1f8b09365c2d065fc1dbe9f6354a3ec5963
himeraoO <himer@bk.ru>
2022-05-19 21:34:52 +0300

    MRTB-0: added SpringBoot skeleton to the project.

---
+++ src/main/java/com/github/himeraoO/mrtb/MobileReviewTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.himeraoO.mrtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileReviewTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileReviewTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/himeraoO/mrtb/MobileReviewTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.himeraoO.mrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileReviewTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
