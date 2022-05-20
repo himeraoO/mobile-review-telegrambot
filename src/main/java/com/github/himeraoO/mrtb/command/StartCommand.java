d907769a212f702557adf3684315d0975c64d1b7
himeraoO <himer@bk.ru>
2022-05-20 22:27:31 +0300

    MRTB-3: added Command pattern for handling Telegram Bot commands

---
+++ src/main/java/com/github/himeraoO/mrtb/command/StartCommand.java
@@ -0,0 +1,27 @@
+package com.github.himeraoO.mrtb.command;
+
+import com.github.himeraoO.mrtb.service.SendBotMessageService;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+/**
+ * Start {@link Command}.
+ */
+public class StartCommand implements Command {
+
+    private final SendBotMessageService sendBotMessageService;
+
+    public final static String START_MESSAGE = "Привет. Я Mobile-review Telegram Bot. Я помогу тебе быть в курсе последних " +
+            "статей, обзоров и новостей.";
+
+    // Здесь не добавляем сервис через получение из Application Context.
+    // Потому что если это сделать так, то будет циклическая зависимость, которая
+    // ломает работу приложения.
+    public StartCommand(SendBotMessageService sendBotMessageService) {
+        this.sendBotMessageService = sendBotMessageService;
+    }
+
+    @Override
+    public void execute(Update update) {
+        sendBotMessageService.sendMessage(update.getMessage().getChatId(), START_MESSAGE);
+    }
+}
---
+++ src/main/java/com/github/himeraoO/mrtb/command/UnknownCommand.java
@@ -0,0 +1,23 @@
+package com.github.himeraoO.mrtb.command;
+
+import com.github.himeraoO.mrtb.service.SendBotMessageService;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+/**
+ * Unknown {@link Command}.
+ */
+public class UnknownCommand implements Command {
+
+    public static final String UNKNOWN_MESSAGE = "Не понимаю вас \uD83D\uDE1F, напишите /help чтобы узнать, что я понимаю.";
+
+    private final SendBotMessageService sendBotMessageService;
+
+    public UnknownCommand(SendBotMessageService sendBotMessageService) {
+        this.sendBotMessageService = sendBotMessageService;
+    }
+
+    @Override
+    public void execute(Update update) {
+        sendBotMessageService.sendMessage(update.getMessage().getChatId(), UNKNOWN_MESSAGE);
+    }
+}
